ba98195742f503b3ebe8a5ca3b4e47208ecd319a
Ilya <64003029+noragamiq@users.noreply.github.com>
2023-08-27 15:05:54 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,67 @@
+// Custom Scripts
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    const nav = document.querySelector('nav')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active')
+            body.classList.add('locked')
+            
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+}
+burgerMenu()
+
+let time = 1800;
+const count = document.querySelector('.time')
+
+setInterval(updateCount, 1000);
+
+function updateCount(){
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    count.innerHTML = `${minutes} : ${seconds}`
+    time--;
+}
+
+// let images = document.querySelectorAll('.paralax__item');
+// new simpleParallax(images);
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
